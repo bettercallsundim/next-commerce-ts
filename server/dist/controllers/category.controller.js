5c0b9ac3b23f1951d564002c2f563e1c2cd79612
sundim <sunmaxboom2@gmail.com>
2024-04-24 01:47:09 +0600

    Remove unused files and code

--- server/dist/controllers/category.controller.js
+++ server/dist/controllers/category.controller.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,13 +17,14 @@ const express_async_handler_1 = __importDefault(require("express-async-handler")
 const Category_model_1 = __importDefault(require("../models/Category.model"));
 const cloudinary_1 = require("../utils/cloudinary");
 const errorHandler_1 = __importDefault(require("../utils/errorHandler"));
-exports.createCategoryWithMulter = (0, express_async_handler_1.default)(async (req, res, next) => {
-    const oldCat = await Category_model_1.default.findOne({ name: req.body.name });
+exports.createCategoryWithMulter = (0, express_async_handler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    const oldCat = yield Category_model_1.default.findOne({ name: req.body.name });
     if (oldCat) {
         throw new errorHandler_1.default(400, "Category already exists");
     }
     if (req.file && req.file.path) {
-        await (0, cloudinary_1.cloudinaryUpload)(req?.file?.path, "category").then(async (resp) => {
+        yield (0, cloudinary_1.cloudinaryUpload)((_a = req === null || req === void 0 ? void 0 : req.file) === null || _a === void 0 ? void 0 : _a.path, "category").then((resp) => __awaiter(void 0, void 0, void 0, function* () {
             if (resp.secure_url) {
                 const { name, description } = req.body;
                 const newCat = new Category_model_1.default({
@@ -25,7 +35,7 @@ exports.createCategoryWithMulter = (0, express_async_handler_1.default)(async (r
                         public_id: resp.public_id,
                     },
                 });
-                await newCat.save();
+                yield newCat.save();
                 res.status(200).json({
                     message: "Category created successfully",
                     data: newCat,
@@ -34,18 +44,18 @@ exports.createCategoryWithMulter = (0, express_async_handler_1.default)(async (r
             else {
                 throw new errorHandler_1.default(400, "Category creation error");
             }
-        });
+        }));
     }
     else {
         throw new errorHandler_1.default(400, "Category creation error");
     }
-});
-exports.createCategory = (0, express_async_handler_1.default)(async (req, res, next) => {
+}));
+exports.createCategory = (0, express_async_handler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const { name, description, icon: { url, public_id }, } = req.body;
     if (!name || !description || !url || !public_id) {
         throw new errorHandler_1.default(400, "All fields are required");
     }
-    const oldCat = await Category_model_1.default.findOne({ name });
+    const oldCat = yield Category_model_1.default.findOne({ name });
     if (oldCat) {
         throw new errorHandler_1.default(400, "Category already exists");
     }
@@ -57,31 +67,31 @@ exports.createCategory = (0, express_async_handler_1.default)(async (req, res, n
             public_id,
         },
     });
-    await newCat.save();
+    yield newCat.save();
     res.status(200).json({
         message: "Category created successfully",
         data: newCat,
     });
-});
-exports.getAllCategories = (0, express_async_handler_1.default)(async (req, res, next) => {
-    const categories = await Category_model_1.default.find();
+}));
+exports.getAllCategories = (0, express_async_handler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const categories = yield Category_model_1.default.find();
     res.status(200).json({
         success: true,
         data: categories,
     });
-});
-exports.deleteCategory = (0, express_async_handler_1.default)(async (req, res, next) => {
-    const findCat = await Category_model_1.default.findById(req.body._id);
-    if (findCat && findCat.icon && findCat.icon.public_id) {
-        await (0, cloudinary_1.deleteCloudinaryUpload)(findCat.icon.public_id).then(async () => {
-            await Category_model_1.default.findByIdAndDelete(req.body._id);
+}));
+exports.deleteCategory = (0, express_async_handler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const findCategory = yield Category_model_1.default.findById(req.body._id);
+    if (findCategory && findCategory.icon && findCategory.icon.public_id) {
+        yield (0, cloudinary_1.deleteCloudinaryUpload)(findCategory.icon.public_id).then(() => __awaiter(void 0, void 0, void 0, function* () {
+            yield Category_model_1.default.findByIdAndDelete(req.body._id);
             return res.status(200).json({
                 success: true,
                 message: "Category removed",
             });
-        });
+        }));
     }
     else {
         throw new errorHandler_1.default(400, "Opps, Category delete error, Not found");
     }
-});
+}));
