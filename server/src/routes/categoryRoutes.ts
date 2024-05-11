10316a0cb7730b8ad699365fbd0aaa5a27b42a5c
sundim <sunmaxboom2@gmail.com>
2024-05-11 16:39:33 +0600

    improvie category system

--- server/src/routes/categoryRoutes.ts
+++ server/src/routes/categoryRoutes.ts
@@ -2,6 +2,7 @@ import {
   createCategory,
   deleteCategory,
   getAllCategories,
+  getAllCategoriesTree,
 } from "../controllers/category.controller";
 import { roleCheck } from "../middleware/auth";
 import express from "express";
@@ -10,6 +11,7 @@ const router = express.Router();
 // router.post("/create", multer("category").single("icon"), createCategoryWithMulter);
 router.post("/create", roleCheck("admin"), createCategory);
 router.get("/all", getAllCategories);
+router.get("/all/tree", getAllCategoriesTree);
 router.delete("/delete", roleCheck("admin"), deleteCategory);
 
 export default router;
